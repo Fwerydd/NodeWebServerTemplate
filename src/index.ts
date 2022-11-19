b4eb46593075f8bd7c7ba9e2b7bd7006b8e09268
fr-froussel <4551879+fr-froussel@users.noreply.github.com>
2022-11-19 11:46:16 +0100

    Add Express loader

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,18 @@
-import bodyParser from 'body-parser';
-import cors from 'cors';
 import express from 'express'
-import helmet from 'helmet';
-import morgan from 'morgan';
 
 import config from './config/index';
+import expressLoader from './loaders/express';
 
-const app = express();
+async function startServer() {
+    const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ title: 'Hello, world (again)!' });
-});
+    // Loaders
+    await expressLoader(app);
 
-app.get('/status', (req, res) => { res.status(200).end(); });
-app.head('/status', (req, res) => { res.status(200).end(); });
-app.enable('trust proxy');
+    // Launch the server
+    app.listen(config.port, () => {
+        console.log(`Server listening on port ${config.port}.`);
+    });
+}
 
-// adding Helmet to enhance your Rest API's security
-app.use(helmet());
-
-// using bodyParser to parse JSON bodies into JS objects
-app.use(bodyParser.json());
-
-// enabling CORS for all requests
-app.use(cors());
-
-// adding morgan to log HTTP requests
-app.use(morgan('combined'));
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.listen(config.port);
+startServer();
